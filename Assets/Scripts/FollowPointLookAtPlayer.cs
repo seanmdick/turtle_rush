13ef639eedb5e3e1284c25daca64f30ab5a7d001
Sean Dick <sean@seandick.net>
2017-01-20 19:29:36 -0800

    now will it work?

---
+++ Assets/Scripts/FollowPointLookAtPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPointLookAtPlayer : MonoBehaviour {
+	public Transform lookTarget;
+	public Vector3 targetOffset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, lookTarget.position + targetOffset, Time.deltaTime);
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Vector3.Normalize(lookTarget.position- transform.position)), Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/SpaceToMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceToMove : MonoBehaviour {
+	private float direction = 1f;
+	private Rigidbody rb;
+	public float offset;
+	public float mag;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			direction = -direction;
+			rb.AddForceAtPosition((transform.forward + transform.up*0.1f) * mag, transform.position + transform.forward + transform.right * direction * offset, ForceMode.Impulse);
+		}
+	}
+}
