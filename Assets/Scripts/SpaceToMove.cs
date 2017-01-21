2b14d00e8e0c2f8836fe0fd068f49751d2df33c5
Sean Dick <sean@seandick.net>
2017-01-20 20:37:47 -0800

    fuxed movement and added a turtle prefab

--- Assets/Scripts/SpaceToMove.cs
+++ Assets/Scripts/SpaceToMove.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpaceToMove : MonoBehaviour {
-	private float direction = 1f, moveTimer;
+	private float direction = 1f, moveTimer, powerTimer;
 	private Rigidbody rb;
 	public float offset;
-	public float mag, flipMag;
+	public float mag, flipMag, maxPower;
 
 	private bool isGrounded, moveHit;
 	// Use this for initialization
@@ -16,7 +16,10 @@ public class SpaceToMove : MonoBehaviour {
 
 	void Update() {
 		moveTimer -= Time.deltaTime;
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKey(KeyCode.Space)) {
+			powerTimer = Mathf.Min(powerTimer + Time.deltaTime, maxPower);
+		}
+		if (Input.GetKeyUp(KeyCode.Space))
 			moveHit = true;
 	}
 	
@@ -24,17 +27,17 @@ public class SpaceToMove : MonoBehaviour {
 	void FixedUpdate () {
 		isGrounded = Physics.Raycast(transform.position, -Vector3.up, 0.5f);
 		if (moveTimer < 0f && isGrounded && moveHit) {
+			float power = Mathf.Max((powerTimer/maxPower), 0.3f);
 			moveTimer = 0.3f;
 			moveHit = false;
 			float dotUp = Vector3.Dot(-transform.up, Vector3.up);
 			if (dotUp > 0.2f) {
-				rb.AddForceAtPosition((-transform.up*0.1f) * flipMag, transform.position + transform.right * direction * offset, ForceMode.Impulse);
+				rb.AddForceAtPosition((-transform.up*0.1f) * flipMag * power, transform.position + transform.right * direction * offset, ForceMode.Impulse);
 			} else {
 				direction = -direction;
-				rb.AddForceAtPosition((transform.forward + transform.up*0.1f) * mag, transform.position + transform.forward + transform.right * direction * offset, ForceMode.Impulse);
+				rb.AddForceAtPosition((transform.forward + transform.up*0.1f) * mag * power, transform.position + transform.forward + transform.right * direction * offset, ForceMode.Impulse);
 			}
-
-			Debug.Log(dotUp);
+			powerTimer = 0f;
 
 		}
 
