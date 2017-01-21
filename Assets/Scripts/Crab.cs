71a8fb4873283013c9dd0b3d5c4cbf080526cc36
Eric N <cheesecow007@gmail.com>
2017-01-21 14:44:25 -0800

    crabby time

---
+++ Assets/Scripts/Crab.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crab : MonoBehaviour {
+	private Vector3 moveTarget;
+	private bool hasTarget;
+	public float minDistance, speed, maxAttackCooldown, crabColSphere;
+	public LayerMask layM;
+	private float attackCooldown;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		attackCooldown -= Time.fixedDeltaTime;
+		if (hasTarget) {
+			moveTowardsTurtle();
+		}
+	}
+
+	public void SetTarget(Vector3 target){
+		if (hasTarget) return;
+		hasTarget = true;
+		moveTarget = target;
+	}
+	
+	void moveTowardsTurtle(){
+		Vector3 direction = moveTarget - transform.position;
+		direction.y = 0;
+		
+		float zMove = moveTarget.z - transform.position.z;
+		float xMove = moveTarget.x - transform.position.x;
+		transform.Translate(direction.normalized * Time.fixedDeltaTime * speed, Space.World);
+
+		// Reached turtle rest then chase again
+		if (Vector3.Distance(transform.position, moveTarget) < minDistance && attackCooldown < 0f){
+			attackCooldown = maxAttackCooldown;
+			// check for hitting turtle
+			Collider[] hitColliders = Physics.OverlapSphere(transform.position, crabColSphere, layM);
+			if (hitColliders.Length > 0){
+				turtleHit(hitColliders[0]);
+			}
+			hasTarget = false;
+		} else {
+			transform.rotation = Quaternion.Slerp(
+				transform.rotation, 
+				Quaternion.LookRotation(Vector3.Normalize(direction)), 
+				Time.fixedDeltaTime
+			);
+		}
+	}
+
+	void turtleHit(Collider col){
+		Debug.Log("hit");
+		col.attachedRigidbody.AddForce(transform.forward * 10, ForceMode.Impulse);
+	}
+}
---
+++ Assets/Scripts/CrabZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabZone : MonoBehaviour {
+	// Use this for initialization
+	// private bool turtleEntered = false;
+	public Crab crab;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+	void OnTriggerStay(Collider col) {
+		Turtle turtle = col.gameObject.GetComponent<Turtle>();
+		if (turtle != null) {
+			crab.SetTarget(col.transform.position);
+		}
+	}
+
+}
