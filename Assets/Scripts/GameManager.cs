a046a725516c7114618cbe68d5a5ae96f40f8f3b
Eric N <cheesecow007@gmail.com>
2017-01-22 14:03:29 -0800

    menus!!!!

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	private UnityEngine.SceneManagement.Scene currentScene;
+	// private [] ;
+	public void LoadLevel(string name){
+		SceneManager.LoadScene(name);
+	}
+
+	// Use this for initialization
+	void Start () {
+		foreach (Selectable selectableUI in Selectable.allSelectables) 
+		{
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Input.GetKeyDown(KeyCode.DownArrow);
+		Input.GetKeyDown(KeyCode.UpArrow);		
+	}
+
+	void NextMenu() {
+
+	}
+}
